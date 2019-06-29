24d89621f9c2ff12af0aedb8a9ae11ed57f43059
Curtis Jochen <curtis.jochen@gmail.com>
2019-06-29 13:41:26 -0400

    Input a hack for issues with the api call where HTTPS wouldn't work yet HTTP would and vise versa

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,13 +17,13 @@ function renderButtons() {
 
 $("#add-button").on("click", function (event) {
   event.preventDefault();
-  if($('#button-input').val() !== ""){
-  var userTopic = $("#button-input").val().trim();
+  if ($('#button-input').val() !== "") {
+    var userTopic = $("#button-input").val().trim();
 
-  topics.push(userTopic);
+    topics.push(userTopic);
 
-  renderButtons();
-}
+    renderButtons();
+  }
 });
 
 $("body").on("click", ".gif", function () {
@@ -43,6 +43,9 @@ $("body").on("click", "button", function () {
   var topic = $(this).attr("data-name");
 
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    topic + "&api_key=tKevWU5Ykf52GJn5Joatr6xVqrNNQIM5&limit=10";
+
+  var queryURL1 = "http://api.giphy.com/v1/gifs/search?q=" +
     topic + "&api_key=tKevWU5Ykf52GJn5Joatr6xVqrNNQIM5&limit=10";
 
   $.ajax({
@@ -70,6 +73,42 @@ $("body").on("click", "button", function () {
 
         $("#gifs-appear-here").prepend(gifDiv);
         console.log(response);
+
+
+
+
+
+      }
+    });
+
+  $.ajax({
+    url: queryURL1,
+    method: "GET"
+  })
+    .then(function (response) {
+      $('#gifs-appear-here').empty();
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+        var gifDiv = $("<div id='gif-size'>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var personImage = $("<img>");
+
+        personImage.attr({ 'src': results[i].images.fixed_height_still.url, 'data-still': results[i].images.fixed_height_still.url, 'data-animate': results[i].images.fixed_height.url, 'data-state': 'still', 'class': 'gif' });
+
+        gifDiv.append(p);
+        gifDiv.append(personImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+        console.log(response);
+
+
+
 
 
       }
